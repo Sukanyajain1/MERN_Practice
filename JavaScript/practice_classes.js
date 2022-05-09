b160689d6c49a537e3f31012f6d23225bc9e69b2
Sukanya Jain <sukanyajain1@mac-studio.lan>
2022-05-08 23:58:03 -0400

    cleaning up

--- JavaScript/practice_classes.js
+++ JavaScript/practice_classes.js
@@ -1,11 +1,11 @@
-// Create a Ninja class
+// Create a User class
 // add an attribute: name
 // add an attribute: health
 // add an attribute: speed - give a default value of 3
 // add an attribute: strength - give a default value of 3
-// add a method: sayName() - This should log that Ninja's name to the console
-// add a method: showStats() - This should show the Ninja's name, strength, speed and health
-// add a method: drinkSake() - This should add +10 Health to the Ninja
+// add a method: sayName() - This should log that User's name to the console
+// add a method: showStats() - This should show the User's name, strength, speed and health
+// add a method: drinkJuice() - This should add +10 Health to the User
 
 class User{
     constructor(name, health){
