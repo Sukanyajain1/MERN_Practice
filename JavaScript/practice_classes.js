3dd912453e100c32be1349a17762e49f5ceb1f70
Sukanya Jain <sukanyajain1@mac-studio.lan>
2022-05-09 03:01:19 -0400

    practicing how to extend classes and inhert properties/methods from a parent class.

--- JavaScript/practice_classes.js
+++ JavaScript/practice_classes.js
@@ -8,7 +8,7 @@
 // add a method: drinkJuice() - This should add +10 Health to the User
 
 class User{
-    constructor(name, health){
+    constructor(name){
         this.name = name;
         this.health = 0;
         this.speed = 3;
@@ -32,10 +32,50 @@ class User{
 }
 
 
-const user1 = new User("Sukanya");
-user1.sayName();
+// const user1 = new User("Sukanya");
+// user1.sayName();
+// console.log("================================")
+// user1.showStats();
+// user1.drinkJuice();
+// console.log("================================")
+// user1.showStats();
+
+
+
+// should have default:
+// 200 health
+// 10 speed
+// 10 strength
+
+// should have new attribute called wisdom: default 10
+
+// methods:
+// speakWisdom() - calls the drinkJuice method from the User class
+// then console.logs a wise message
+class Teacher extends User {
+    constructor (name) {
+        super(name);
+        this.health = 200;
+        this.speed = 10;
+        this.strength = 10;
+        this.wisdom = 10;
+    }
+
+    speakWisdom(){
+        super.drinkJuice();
+        console.log("This is a wise message from your teacher:");
+        console.log("Shopping for groceries on an empty stomach doesn't always result in the greatest haul.");
+        console.log("Have a snack and clear your head before you head out the door.");
+    }
+}
+
+
+
+const teacher1 = new Teacher("Sukanya's Mom");
+teacher1.sayName();
 console.log("================================")
-user1.showStats();
-user1.drinkJuice();
+teacher1.showStats();
+teacher1.drinkJuice();
 console.log("================================")
-user1.showStats();
+teacher1.showStats();
+teacher1.speakWisdom();
