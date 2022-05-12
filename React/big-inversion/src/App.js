990e13f23a9b87e0133df1668b503ece09b5621c
Sukanya Jain <sukanyajain1@mac-studio.lan>
2022-05-12 16:36:55 -0400

    trying same react app as putting it together, but replacing class component with functional component

--- React/big-inversion/src/App.js
+++ React/big-inversion/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <PersonCard lastName ="Doe" firstName ="Jane" age ={45} hairColor ="Black"/>
       <PersonCard lastName ="Smith" firstName ="John" age ={88} hairColor ="Brown"/>
-      <PersonCard lastName ="Fillmroe" firstName ="Millard" age ={50} hairColor ="Brown"/>
+      <PersonCard lastName ="Fillmore" firstName ="Millard" age ={50} hairColor ="Brown"/>
       <PersonCard lastName ="Smith" firstName ="Maria" age ={62} hairColor ="Brown"/>
     </div>
   );
