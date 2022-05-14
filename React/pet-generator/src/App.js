e08ec883657be45f0c6d4b96f62e092c8ff0b62b
Sukanya Jain <sukanyajain1@mac-studio.lan>
2022-05-13 21:44:46 -0400

    pet generator with minor css changes

--- React/pet-generator/src/App.js
+++ React/pet-generator/src/App.js
@@ -1,4 +1,4 @@
-// import './App.css';
+import './App.css';
 
 import PetForm from "./components/PetForm";
 
--- React/pet-generator/src/components/PetForm.jsx
+++ React/pet-generator/src/components/PetForm.jsx
@@ -7,6 +7,7 @@ const PetForm = () => {
     let [numWins, setNumWins] = useState("");
     let [profPic, setProfPic] = useState("");
     let [power, setPower] = useState("");
+    let [favColor, setFavColor] = useState("");
     
 
     // create state variable which is an array so that we can store all the pets that are submitted.
@@ -23,12 +24,20 @@ const PetForm = () => {
         
         // every time a pet gets created, we want to package it into an object.
         // create a pet object (python dictionary) -> also called a hashmap
-        let pet = {name, numWins, profPic, power}
+        let pet = {name, numWins, profPic, power, favColor}
         
         // use the setPetList setter to update the petList array to have the pet object inside of it
         setPetList([...petList, pet])
-        // console.log("pet looks like this--->", pet)
-        // console.log("Pet List: ", petList)
+        
+        
+        // clear out the stat variables so that we can use this to empty the form inputs
+        // set the variables to nothing after appending them to the list
+        // then add the variables to the input tags using an html value attribute.
+        setName("");
+        setNumWins("");
+        setProfPic("");
+        setPower("");
+        setFavColor("");
         
     }
 
@@ -44,38 +53,44 @@ const PetForm = () => {
             <form onSubmit={submitPet}>
                 <div className="form-group">
                     <label htmlFor=""> Name of Pet: </label>
-                    <input onChange={(e) => {setName(e.target.value)}} type="text" className="form-control" />
+                    <input onChange={(e) => {setName(e.target.value)}} type="text" className="form-control" value = {name}/>
                 </div>
                 <div className="form-group">
                     <label htmlFor=""> Number of Villains Defeated: </label>
-                    <input onChange={(e) => {setNumWins(e.target.value)}} type="number" className="form-control" />
+                    <input onChange={(e) => {setNumWins(e.target.value)}} type="number" className="form-control" value = {numWins}/>
                 </div>
                 <div className="form-group">
                     <label htmlFor=""> Profile picture url: </label>
-                    <input onChange={(e) => {setProfPic(e.target.value)}} type="text" className="form-control" />
+                    <input onChange={(e) => {setProfPic(e.target.value)}} type="text" className="form-control" value = {profPic}/>
                 </div>
                 <div className="form-group">
                     <label htmlFor=""> Super Power: </label>
-                    <input onChange={(e) => {setPower(e.target.value)}} type="text" className="form-control" />
+                    <input onChange={(e) => {setPower(e.target.value)}} type="text" className="form-control" value = {power}/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor=""> Favorite Color: </label>
+                    <input onChange={(e) => {setFavColor(e.target.value)}} type="text" className="form-control" value = {favColor}/>
                 </div>
                 <input type="submit" value="Add Pet" className="btn btn-primary mt-3" />
             </form>
             <hr/>
 
 
-            {
-                petList.map((petObj, idx) =>{
-                    return (
-                        <>
-                            <h1 key={idx}>{petObj.name}</h1>
-                            <img src={petObj.profPic} alt="" width = "400px" height = "auto" />
-                            <h3>Super power: {petObj.power}</h3>
-                            <h4>Number of Villains Defeated: {petObj.numWins}</h4>
-                            <hr/>
-                        </>
-                    );
-                })
-            }
+            <div className = "pet-list">
+                {
+                    petList.map((petObj, idx) =>{
+                        return (
+                                <div className='pet-card' style = {{backgroundColor: petObj.favColor}}>
+                                    <h2 key={idx}>{petObj.name}</h2>
+                                    <p>Number of Villains Defeated: {petObj.numWins}</p>
+                                    <img src={petObj.profPic} alt="" width = "250px" height = "250px" />
+                                    <h4>Super power: </h4>
+                                    <h5>{petObj.power}</h5>
+                                </div>
+                        );
+                    })
+                }
+            </div>
         </>
     );
 }
